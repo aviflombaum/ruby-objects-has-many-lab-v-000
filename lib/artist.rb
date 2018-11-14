53d9355ba60b2806e28bbf4d873cfab70711604a
Avi Flombaum <avi@flombaum.com>
2018-11-14 23:41:11 +0000

    automatically backed up by learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -6,4 +6,12 @@ class Artist
     @songs = []
   end
 
+  #Artist #name #add_song takes in an argument of a song and adds that song to the artist's collection and tells
+  #the song that it belongs to that artist
+  #Failure/Error: adele.add_song(hello)
+  #NoMethodError:
+  # undefined method `add_song' for #<Artist:0x00000001920f30 @name="Adele", @songs=[]>
+  def add_song
+  end
+  
 end
