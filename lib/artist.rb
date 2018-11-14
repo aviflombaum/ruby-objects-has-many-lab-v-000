c8e6732e7bd9a81371f75d436a00bf6dd5d5a88a
Avi Flombaum <avi@flombaum.com>
2018-11-14 23:54:07 +0000

    automatically backed up by learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -7,15 +7,20 @@ class Artist
     @songs = []
   end
 
-  #Artist #name #add_song takes in an argument of a song and adds that song to the artist's collection and tells
-  #the song that it belongs to that artist
-  #Failure/Error: adele.add_song(hello)
-  #NoMethodError:
-  # undefined method `add_song' for #<Artist:0x00000001920f30 @name="Adele", @songs=[]>
   def add_song(song)
     @songs << song
-    
+
     song.artist = self # Reciprocity
+  end
+
+  #   1) Artist #name #add_song_by_name takes in an argument of a song name, creates a new song with it and associates
+  # the song and artist
+  #      Failure/Error: adele.add_song_by_name("Rolling in the Deep")
+  #      NoMethodError:
+  #        undefined method `add_song_by_name' for #<Artist:0x000000012fcfa0 @name="Adele", @songs=[]>
+  #      # ./spec/artist_spec.rb:39:in `block (4 levels) in <top (required)>'
+  def add_song_by_name(song_name)
+    
   end
 
 end
